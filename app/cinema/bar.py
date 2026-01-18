d8c6024854cedeca2863a18236c44d7215bdceaa
noshenko98 <noshenko98@gmail.com>
2026-01-18 17:10:45 +0200

    Solution

---
+++ app/cinema/bar.py
@@ -0,0 +1,5 @@
+class CinemaBar:
+
+    @staticmethod
+    def sell_product(product, customer: "Customer") -> None:
+        print(f"Cinema bar sold {product} to {customer}.")
--- app/main.py
+++ app/main.py
@@ -1,6 +1,16 @@
 # write your imports here
+from app.cinema.bar import CinemaBar
+from app.cinema.hall import CinemaHall
+from app.people.cinema_staff import Cleaner
+from app.people.customer import Customer
 
 
 def cinema_visit(customers: list, hall_number: int, cleaner: str, movie: str):
-    # write you code here
-    pass
+    list_customers = []
+    for customer in customers:
+        list_customers.append(Customer(customer["name"], customer["food"]))
+    hall = CinemaHall(hall_number)
+    cleaner_staff = Cleaner(cleaner)
+    for customer_el in list_customers:
+        CinemaBar.sell_product(customer = customer_el.name, product = customer_el.food)
+    hall.movie_session(movie_name = movie, customers = list_customers, cleaning_staff= cleaner_staff)
